cb060565250b843b29c01b9c5759b402ba837384
harunbansode <harun.bansode@gmail.com>
2025-02-08 17:59:17 -0500

    deleted files

--- checking_alphabet.py
+++
@@ -1,9 +0,0 @@
-# string1 = "Ha@run"
-# def remove_char(string1):
-#     string2 = str()
-#     for i in string1:
-#         if 65 <= ord(i) <= 90 or 97 <= ord(i) <= 122:
-#             string2 = string2 + i
-#     return string2
-#
-# print(remove_char(string1))
